1a375b2cb06b81db3fa555dda96f8976fff2a264
Clark Winkelmann <clark.winkelmann@gmail.com>
2022-09-19 21:30:56 +0200

    Trying to make the routing code more robust

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -6,13 +6,17 @@ import UserPage from 'flarum/forum/components/UserPage';
 import GraphsPage from './components/GraphsPage';
 
 app.initializers.add('clarkwinkelmann-formulaire-profile-graphs', () => {
+    // Define the route here first to make it more resilient against "route does not exist" errors
+    app.routes.formulaireProfileGraphs = {
+        // Intentionally not the same name as the default setting. This one is more unique to make extra sure no route exists with this name
+        path: '/u/:username/formulaire-graphs',
+        component: GraphsPage,
+    };
+
     // Use override because we need to call our code after boot() to get access to app.forum, but before m.route() is called in mount()
     // Similar logic to kilowhat/flarum-ext-custom-paths except we add a new route instead of editing any
     override(ForumApplication.prototype, 'mount', function (original: any, ...args: any[]) {
-        app.routes.formulaireProfileGraphs = {
-            path: '/u/:username/' + app.forum.attribute('formulaire-profile-graphs.path'),
-            component: GraphsPage,
-        };
+        app.routes.formulaireProfileGraphs.path = '/u/:username/' + app.forum.attribute('formulaire-profile-graphs.path');
 
         original.call(this, ...args);
     });
